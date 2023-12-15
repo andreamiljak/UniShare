3ad53b6ae2d5dbae851e95c699975061bbbbb246
Toni Vujic <51680246+tvujic@users.noreply.github.com>
2023-12-15 12:05:52 +0100

    Add files via upload

--- UniShare/pages/ubacivanje_u_bazu.php
+++ UniShare/pages/ubacivanje_u_bazu.php
@@ -7,13 +7,47 @@
 	if ($conn->connect_error){
 		die('Connection failed : '.$conn->connection_error);
 	}
-	else {
-		$stmt = $conn->prepare("insert into korisnici(korisnicko_ime, lozinka, email) values (?, ?, ?)");
-		$stmt->bind_param("sss", $korisnicko_ime, $lozinka, $email);
+	function isUsernameTaken($korisnicko_ime, $conn) {
+		$query = "SELECT * FROM korisnici WHERE korisnicko_ime = ?";
+		$stmt = $conn->prepare($query);
+		$stmt->bind_param("s", $korisnicko_ime);
 		$stmt->execute();
-		echo file_get_contents("materijalihub.html");
-		$stmt->close();
-		$conn->close();
+		$result = $stmt->get_result();
+		return $result->num_rows > 0;
 	}
+	function isEmailTaken($email, $conn) {
+		$query = "SELECT * FROM korisnici WHERE email = ?";
+		$stmt = $conn->prepare($query);
+		$stmt->bind_param("s", $email);
+		$stmt->execute();
+		$result = $stmt->get_result();
+		return $result->num_rows > 0;
+	}
+	if (isUsernameTaken($korisnicko_ime, $conn)) {
+		echo '<script>alert("Profil sa ovim korisnickim imenom vec postoji.");</script>';
+		echo file_get_contents("register.html");
+
+	} 
+	elseif (isEmailTaken($email, $conn)) {
+		echo '<script>alert("Profil sa ovim emailom vec postoji.");</script>';
+		echo file_get_contents("register.html");
+	} 
+	else {
+    // Insert new user into the database
+		$insertQuery = "INSERT INTO korisnici (korisnicko_ime, email, lozinka) VALUES (?, ?, ?)";
+		$insertStmt = $conn->prepare($insertQuery);
+		$insertStmt->bind_param("sss", $korisnicko_ime, $email, $lozinka);
+    
+		if ($insertStmt->execute()) {
+
+			echo "User registered successfully.";
+			echo file_get_contents("materijalihub.html");
+		} 
+		else {
+			echo "Error registering user: " . $insertStmt->error;
+		}
+	}
+
+		$conn->close();
 
 ?>
